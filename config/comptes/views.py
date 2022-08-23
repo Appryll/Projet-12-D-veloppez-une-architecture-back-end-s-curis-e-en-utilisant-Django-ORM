2d44afb93709f872b96eeae7337ee41be224b8c8
Natalia FABIANO <natalia.fabiano@hotmail.com>
2022-08-23 14:04:45 +0200

    filter/serch/order + amelioration panel control django(client)

--- config/comptes/views.py
+++ config/comptes/views.py
@@ -2,16 +2,35 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ClientSerializer, SalesSerializer, SupportSerializer
 from .models import Client, Sales, Support
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated, IsAdminUser 
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 
+
+class ClientFilter(FilterSet):
+    class Meta:
+        model = Client
+        fields = ['last_name', 'email']
 
 class ClientList(viewsets.ModelViewSet):
     serializer_class = ClientSerializer
     queryset = Client.objects.all()
+    permission_classes = [IsAuthenticated]#IsSalesPermissions
+    pagination_class = PageNumberPagination
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('last_name', 'email', 'company_name', 'sales_contact')
+    ordering_fields = ('last_name', 'date_created')
+    filterset_class = ClientFilter
 
 class SalesList(viewsets.ModelViewSet):
     serializer_class = SalesSerializer
     queryset = Sales.objects.all()
+    permission_classes = [IsAuthenticated|IsAdminUser]
+    pagination_class = PageNumberPagination
 
 class SupportList(viewsets.ModelViewSet):
     serializer_class = SupportSerializer
     queryset = Support.objects.all()
+    permission_classes = [IsAuthenticated|IsAdminUser]
+    pagination_class = PageNumberPagination
--- config/contrat/views.py
+++ config/contrat/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ContratSerializer
 from .models import Contrat
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from comptes.views import ClientFilter
 
 class ContratList(viewsets.ModelViewSet):
     serializer_class = ContratSerializer
     queryset = Contrat.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    filterset_fields = ('date_created', 'amount')
+    search_fields = ('date_created', 'amount')
+    ordering_fields = ('date_created', 'amount')
+    filterset_class = ClientFilter
